0df39a2bedc9d28cd459f4e6558c9123024fdf94
taegeun <djqtek0525@naver.com>
2022-11-30 00:55:59 +0900

    [add] Token 디펜던시, 코드 추가

--- src/main/java/com/hospital/review2/service/UserService.java
+++ src/main/java/com/hospital/review2/service/UserService.java
@@ -6,7 +6,10 @@ import com.hospital.review2.domain.dto.UserJoinRequest;
 import com.hospital.review2.exception.ErrorCode;
 import com.hospital.review2.exception.HospitalReviewAppException;
 import com.hospital.review2.repository.UserRepository;
+import com.hospital.utils.JwtTokenUtil;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +19,10 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder encoder;
+
+    @Value("${jwt.token.secret}")
+    private String secretKey;
+    private long expireTimeMs = 1000 * 60 * 60; // 1시간
 
     public UserDto join(UserJoinRequest request) {
         // 비즈니스 로직 - 회원 가입
@@ -40,14 +47,17 @@ public class UserService {
 
         // userName있는지 여부 확인
         // 없으면 NOT_FOUND에러 발생
-        userRepository.findByUserName(userName)
+        User user = userRepository.findByUserName(userName)
                 .orElseThrow(() -> new HospitalReviewAppException(ErrorCode.NOT_FOUND, String.format("%s는 가입된 적이 없습니다.", userName)));
 
         // password일치 하는지 여부 확인
+        if(!encoder.matches(password, user.getPassword())){
+            throw new HospitalReviewAppException(ErrorCode.INVALID_PASSWORD, String.format("userName 또는 password가 잘못 됐습니다."));
+        }
 
         // 두가지 확인중 예외 안났으면 Token발행
-
-        return "";
+        return JwtTokenUtil.createToken(userName, secretKey, expireTimeMs);
     }
+
 
 }
